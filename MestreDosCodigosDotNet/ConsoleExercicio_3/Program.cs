daa304b0197148ef520421cdd722340bdd8be981
Jeferson Parckert <64714048+JefersonParckert@users.noreply.github.com>
2020-05-12 22:50:17 -0300

    Término do execcício 3 - trabalhando no Console

--- MestreDosCodigosDotNet/ConsoleExercicio_3/Program.cs
+++ MestreDosCodigosDotNet/ConsoleExercicio_3/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleExercicio_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Exibindo todos os números multiplos de 3 entre 1 e 100");
+            int numero = 1;
+            while (numero <= 100) 
+            {
+                if ((numero % 3) == 0)
+                    Console.WriteLine($"O número {numero} é multiplo de 3");
+
+                numero++;
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
