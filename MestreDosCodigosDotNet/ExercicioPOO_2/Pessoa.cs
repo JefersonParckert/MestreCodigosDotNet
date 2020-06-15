71f3e37539d92fd06fbe15f249ba46272d6c07f4
Jeferson Parckert <64714048+JefersonParckert@users.noreply.github.com>
2020-06-14 23:19:31 -0300

    Resolução do Exercicio 2 de POO bem como a resolução de alguns exercícios teóricos.

---
+++ MestreDosCodigosDotNet/ExercicioPOO_2/Pessoa.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace ExercicioPOO_2
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private DateTime _dataNascimento;
+        private double _altura;
+
+        public Pessoa()
+        {
+            InicializarDados(string.Empty, DateTime.MinValue, 0.0);
+        }
+
+        public Pessoa(string nome, DateTime dataNascimento, double altura)
+        {
+            InicializarDados(nome, DateTime.MinValue, 0.0);
+
+            DataNascimento = dataNascimento;
+            Altura = altura;
+        }
+
+        private void InicializarDados(string nome, DateTime dataNascimento, double altura) 
+        {
+            _nome = nome;
+            _dataNascimento = dataNascimento;
+            _altura = altura;
+        }
+        
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                _nome = value;
+            }
+        }
+
+        public DateTime DataNascimento
+        {
+            get
+            {
+                return _dataNascimento;
+            }
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new ArgumentOutOfRangeException("A Data de Nascimento deve ser menor que HOJE!");
+
+                _dataNascimento = value;
+            }
+        }
+
+        public double Altura
+        {
+            get
+            {
+                return _altura;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("A altura deve ser maior que ZERO!");
+
+                _altura = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {_nome}, {IdadePessoa()} ano(s) \nData Nascimento.: {_dataNascimento.ToShortDateString()} \nAltura.: {_altura:N2}";
+        }
+
+        public int IdadePessoa() {
+            int idade = (DateTime.Now.Year - _dataNascimento.Year);
+            if (DateTime.Now.Month < _dataNascimento.Month)
+                idade--;
+
+            if ((DateTime.Now.Month == _dataNascimento.Month) && (DateTime.Now.Day < _dataNascimento.Day))
+                idade--;
+
+            return idade;
+        }
+    }
+}
---
+++ MestreDosCodigosDotNet/ExercicioPOO_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExercicioPOO_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                var pessoa1 = new Pessoa();
+                pessoa1.Nome = "Jeferson";
+                pessoa1.Altura = 1.8;
+                pessoa1.DataNascimento = new DateTime(1984, 03, 02);
+                Console.WriteLine(pessoa1);
+
+                Console.WriteLine();
+                var pessoa2 = new Pessoa("Vanessa", new DateTime(1984, 10, 06), 1.68);
+                Console.WriteLine(pessoa2);
+
+            try
+            { 
+                Console.WriteLine();
+                var pessoa3 = new Pessoa("Bianca", new DateTime(2021, DateTime.Now.Month, DateTime.Now.Day), 1.20);
+                Console.WriteLine(pessoa3);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine("Erro ao Criar a 3ª pessoa.");
+                Console.WriteLine($"ERRO.: {e.Message}");
+            }
+        }
+    }
+}
