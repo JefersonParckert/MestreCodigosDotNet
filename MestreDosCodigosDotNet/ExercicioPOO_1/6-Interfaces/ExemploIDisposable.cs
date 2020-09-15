de0bded31bf0d81f74067e2302db9618e2516e8c
Jeferson <jeferson.parckert@gmail.com>
2020-09-14 23:20:53 -0300

    Implementação da interface IDisposable

--- MestreDosCodigosDotNet/ExercicioPOO_1/6-Interfaces/ExemploIDisposable.cs
+++ MestreDosCodigosDotNet/ExercicioPOO_1/6-Interfaces/ExemploIDisposable.cs
@@ -2,12 +2,19 @@
 
 namespace ExercicioPOO_1
 {
+    /*A principal utilização da interface IDisposible é liberar recursos não gerenciados, permitindo o uso do using */
     public static class ExemploIDisposable
     {
         public static void Executar()
         {
             Console.WriteLine("========== IDisposable ===========");
-
+            Console.WriteLine("Gravando logs! Por favor aguarde!");
+            using (Log log = new Log($"ArquivoLog_{Guid.NewGuid()}.txt"))
+            {
+                log.GravarLog(" Primeiro Log ");
+                log.GravarLog(" Segundo Log ");
+            }
+            Console.WriteLine("Logs Gravados com sucesso!");
             Console.WriteLine();
         }
     }
---
+++ MestreDosCodigosDotNet/ExercicioPOO_1/6-Interfaces/Log.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ExercicioPOO_1
+{
+    public class Log: IDisposable
+    {
+        private readonly FileStream _fileStream;
+
+        public Log(string nomeArquivo)
+        {
+            _fileStream = new FileStream(nomeArquivo, FileMode.OpenOrCreate, FileAccess.Write);
+        }
+
+        public void GravarLog(string log)
+        {
+            byte[] dados = Encoding.ASCII.GetBytes(log);
+
+            _fileStream.Write(dados, 0, dados.Length);
+        }
+
+        public void Dispose()
+        {
+            _fileStream?.Close();
+            _fileStream?.Dispose();
+        }
+    }
+}
