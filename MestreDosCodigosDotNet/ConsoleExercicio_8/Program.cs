7c1199bf80d0b8439e53e6c9d5ea2ff70b6c374a
Jeferson <jeferson.parckert@gmail.com>
2020-10-15 12:14:55 -0300

    Ajuste Banca, Exercício Console 8,  Remover métodos desnecessários

--- MestreDosCodigosDotNet/ConsoleExercicio_8/Program.cs
+++ MestreDosCodigosDotNet/ConsoleExercicio_8/Program.cs
@@ -1,7 +1,6 @@
 ﻿using ConsoleExercicio_8.Util;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace ConsoleExercicio_8
 {
@@ -13,23 +12,23 @@ namespace ConsoleExercicio_8
             List<double> listaNumeros = new List<double>();
             while (true)
             {
-                LimparTela();
+                Console.Clear();
 
                 //metodos de ordenação disponível nos métodos de extensão
-                ImprimirTexto($"Situação da Lista.: [{listaNumeros.ToLista(Constantes.Ordenacao.SemOrdenacao)}]");
-                ImprimirTexto($"Lista em Ordem Crescente: [{listaNumeros.ToLista(Constantes.Ordenacao.Crescente)}]");
-                ImprimirTexto($"Lista em Ordem Decrescente: [{listaNumeros.ToLista(Constantes.Ordenacao.Decrescente)}]\r\n");
+                Console.WriteLine($"Situação da Lista.: [{listaNumeros.ToLista(Constantes.Ordenacao.SemOrdenacao)}]");
+                Console.WriteLine($"Lista em Ordem Crescente: [{listaNumeros.ToLista(Constantes.Ordenacao.Crescente)}]");
+                Console.WriteLine($"Lista em Ordem Decrescente: [{listaNumeros.ToLista(Constantes.Ordenacao.Decrescente)}]\r\n");
 
-                ImprimirTexto(menu.PegarOpcoesMenu());
+                Console.WriteLine(menu.PegarOpcoesMenu());
                 
                 string opcaoDigitada = Console.ReadLine();
 
                 Menu.OpcoesMenu opcaoSelecionada;
                 if (!menu.TestarOpcaoCadastrada(opcaoDigitada, out opcaoSelecionada)) 
                 {
-                    LimparTela();
-                    ImprimirTexto("Opção Inválida. Pressione ENTER para continuar!");
-                    AguardarUsuario();
+                    Console.Clear();
+                    Console.WriteLine("Opção Inválida. Pressione ENTER para continuar!");
+                    Console.ReadKey();
                 }
 
                 if (opcaoSelecionada == Menu.OpcoesMenu.Sair)
@@ -46,53 +45,28 @@ namespace ConsoleExercicio_8
 
             while (true)
             {
-                LimparTela();
-                ImprimirTexto("Informe o número desejado!");
-                string numeroDigitado = PegarOpcaoUsuario();
+                Console.Clear();
+                Console.WriteLine("Informe o número desejado!");
+                string numeroDigitado = Console.ReadLine();
                 if (numeroDigitado.DoubleValido())
                 {
                     listaNumeros.Add(Convert.ToDouble(numeroDigitado));
                     break;
                 }
 
-                LimparTela();
-                ImprimirTexto("Número inválido! Pressione ENTER para continuar");
-                AguardarUsuario();
+                Console.Clear();
+                Console.WriteLine("Número inválido! Pressione ENTER para continuar");
+                Console.ReadKey();
             }
-        }
-
-        private static void AguardarUsuario()
-        {
-            Console.ReadKey();
-        }
-
-        private static void LimparTela() 
-        {
-            Console.Clear();
-        }
-
-        private static void ImprimirTexto(string texto)
-        {
-            Console.WriteLine(texto);
-        }
-
-        private static string PegarOpcaoUsuario()
-        { 
-            return Console.ReadLine();
         }
 
         private static Menu PegarMenu()
         { 
             var menu = new Menu();
-            PopularMenu(menu);
-
-            return menu;
-        }
-
-        private static void PopularMenu(Menu menu)
-        {
             menu.AdicionarItem("1", "Adicionar número");
             menu.AdicionarItem("0", "Sair");
-        }
+
+            return menu;
+        }       
     }
 }
