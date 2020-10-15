4fd39613baad3385f2e6b82f022ffac677e53698
Jeferson <jeferson.parckert@gmail.com>
2020-10-15 11:58:55 -0300

    Ajuste banca,  exercício Console 5 - Disponibilizar opção para o usuário informar os valores de a,b e c para o calculo de bhascara

--- MestreDosCodigosDotNet/ConsoleExercicio_5/Program.cs
+++ MestreDosCodigosDotNet/ConsoleExercicio_5/Program.cs
@@ -1,33 +1,47 @@
 ﻿using ConsoleExercicio_5.RegraNegocio;
+using ConsoleExercicio_5.Uteis;
 using System;
 
 namespace ConsoleExercicio_5
 {
     class Program
     {
+        private const double NUMERO_INDEFINIDO = -9999.99;
         static void Main(string[] args)
         {
             FormulaBhaskara bhaskara = new FormulaBhaskara();
 
-            //DELTA maior que ZERO
-            Console.WriteLine("Calculando (a = 1, b = -1, c = -6)");
-            bhaskara.Calcular(1, -1, -6);
-            Console.WriteLine(bhaskara.Resultado);
-            Console.WriteLine();
+            var valorA = PegarValorParametro("a");
+            var valorB = PegarValorParametro("b");
+            var valorC = PegarValorParametro("c");
 
-            //DELTA igual que ZERO
-            Console.WriteLine("Calculando (a = 1, b = -8, c = 16)");
-            bhaskara.Calcular(1, -8, 16);
+            Console.WriteLine($"Calculando (a = {valorA}, b = {valorB}, c = {valorC})");            
+            
+            bhaskara.Calcular(valorA, valorB, valorC);
+            
             Console.WriteLine(bhaskara.Resultado);
-            Console.WriteLine();
+            Console.ReadKey();            
+        }
 
-            //DELTA menor que ZERO
-            Console.WriteLine("Calculando (a = 1, b = -8, c = 16)");
-            bhaskara.Calcular(-1, 2, -2);
-            Console.WriteLine(bhaskara.Resultado);
-            Console.WriteLine();
 
-            Console.ReadKey();
+        private static double PegarValorParametro(string nomeParametro)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Favor informar o valor do parâmetro [{nomeParametro}]");
+
+                var valor = Console.ReadLine().ToDouble(NUMERO_INDEFINIDO);
+
+                var entradaCorreta = (valor != NUMERO_INDEFINIDO);
+                if (entradaCorreta)
+                    return valor;
+
+                Console.WriteLine("O número informado é inválido, favor informar um número válido!");
+                Console.ReadKey();
+
+            } while (true);
+
         }
     }
 }
---
+++ MestreDosCodigosDotNet/ConsoleExercicio_5/Uteis/Extensoes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleExercicio_5.Uteis
+{
+    public static class Extensoes
+    {
+        public static double ToDouble(this object valor, double defVal)
+        {
+            try
+            {
+                return Convert.ToDouble(valor);
+            }
+            catch
+            {
+                return defVal;
+            }
+        }
+    }
+}
